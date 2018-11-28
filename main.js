b0ba41c77945043775d23df035cf36c3923b96b5
Sudharshan <sudhar393@gmail.com>
2018-11-28 19:41:24 +0800

    progress

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ var hex_map = {
 var colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
 var questions_matrix = [
-    ['red q1', 'red q2', 'red q3'],
-    ['orange q1', 'orange q2', 'orange q3'],
-    ['yellow q1', 'yellow q2', 'yellow q3'],
-    ['green q1', 'green q2', 'green q3'],
-    ['blue q1', 'blue q2', 'blue q3'],
-    ['purple q1', 'purple q2', 'purple q3'],
+    ['Are you ambitious?', 'Do you think you are aggressive?', 'Are you proud of yourself?'],
+    ['Do you consider yourself chaotic?', 'Do you think you are extra?', 'Are you unpredictable?'],
+    ['Are you optimistic?', 'Do you think you are loud?', 'Do people tell you you are annoying?'],
+    ['Do you think that life sucks sometimes?', 'Are you often taking challenges?', 'Would you do anything to get what you want?'],
+    ['Are you focused on what you want?', 'Do you zone people out often?', 'Are you patient for rewards?'],
+    ['Are you content with your life now?', 'Do you take what you get?', 'Would you do anything for someone you care about?'],
 ]
 
 // ---------------------- STATE ---------------------------------------- //
 var page = '.one'
 var lastpage = '.two'
-var currentlevel = 0
+var currentquestion = 0
 var currentcolor = 0 // In terms of colours list index
 
 var points_map = {
@@ -33,18 +33,16 @@ var points_map = {
     'purple': 0,
 }
 
-var shortlist = [] // Shortlisted colours
-
 // -------------------- UTILITIES --------------------- //
 function pageSwitch(selector1, selector2) {
      anime({
         targets: selector1,
         left: [
             { value: '50%', duration: 0 },
-            { value: '20%', duration: 2000 }
+            { value: '30%', duration: 1000 }
         ],
         opacity: 0,
-        duration: 2000,
+        duration: 500,
         elasticity: 0,
         loop: false,
         complete: function(anim) {
@@ -55,11 +53,11 @@ function pageSwitch(selector1, selector2) {
       anime({
         targets: selector2,
         left: [
-            { value: '100%', duration: 0 },
-            { value: '50%', duration: 2000 }
+            { value: '70%', duration: 0 },
+            { value: '50%', duration: 1000 }
         ],
         opacity: 1,
-        duration: 2000,
+        duration: 500,
         elasticity: 0,
         loop: false,
         begin: function(anim) {
@@ -74,17 +72,32 @@ function nextQuestion(question) {
         lastpage = page
         page = '.two'
         document.querySelector('#q2').innerText = question
+        console.log('hi page 2')
     } else if(page === '.two') {
         lastpage = page
         page = '.one'
         document.querySelector('#q1').innerText = question
+        console.log('hi page 1')
     }
 
-    pageSwitch(page, lastpage)
+    pageSwitch(lastpage, page)
 }
 
 function gameStep(isTrue) {
-   
+    points_map[colours[currentcolor]] += isTrue // Add the point
+
+    if(currentcolor < 5) {
+        currentcolor++
+    } else {
+        currentcolor = 0
+
+        if(currentquestion < 3) {
+            currentquestion++
+        } else {
+            // Endgame
+        }
+    }
+    console.log(points_map)
 }
 // ----------------------------------------------------- MAIN CODE ------------------------------------------------- //
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -94,15 +107,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Triggers the animated background
     var cnt = 1
     var colorswitch = setInterval(function() {
-        console.log("DOM fully loaded and parsed");
         document.querySelector('body').style.background = hex_map[colours[cnt]]
-        document.querySelector('#start').style.color = hex_map[colours[cnt]]
+        document.querySelectorAll('.back')[0].style.color = hex_map[colours[cnt]]
         cnt = (cnt + 1) % 6
-    }, 5000)
+    }, 3000)
 
     document.querySelector('#start').addEventListener('click', function() {
-        clearInterval(colorswitch)
-        // Start actual game phase
+        document.querySelector('#q1').innerText = questions_matrix[currentcolor][currentquestion]
         pageSwitch('.main', '.question.one')
     })
+
+    document.querySelector('.yes.one').addEventListener('click', function() {
+        gameStep(1)
+        nextQuestion(questions_matrix[currentcolor][currentquestion])
+    })
+
+    document.querySelector('.yes.two').addEventListener('click', function() {
+        gameStep(1)
+        nextQuestion(questions_matrix[currentcolor][currentquestion])
+    })
+
+    document.querySelector('.no.one').addEventListener('click', function() {
+        gameStep(0)
+        nextQuestion(questions_matrix[currentcolor][currentquestion])
+    })
+
+    document.querySelector('.no.two').addEventListener('click', function() {
+        gameStep(0)
+        nextQuestion(questions_matrix[currentcolor][currentquestion])
+    })
+
   })
